92df2b202cdec42e463ac6a54e2ed6939bdaadee
Rafael Silva <carlosrafael_silva@yahoo.com.br>
2021-07-20 12:41:15 -0300

    Criação do arquivo principal com o metodo main

---
+++ TesteFatorial.java
@@ -0,0 +1,7 @@
+class TesteFatorial {
+	public static void main(String[] args) {
+		Fatoria fat = new Fatorial();
+		int resultado = fat.calcularFatorial(5);
+		system.out.println(resultado);
+	}
+}
